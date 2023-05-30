d2be5715b9860bb084c577b167b0c033de44227d
paul-kang-1 <loiter97@gmail.com>
2023-05-30 10:21:27 -0400

    added qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,57 @@
+"""Qutebrowser configuration"""
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401,E501 pylint: disable=unused-import
+from qutebrowser.config.config import ConfigContainer  # noqa: F401,E501 pylint: disable=unused-import
+config = config  # type: ConfigAPI # noqa: F821 pylint: disable=E0602,C0103
+c = c  # type: ConfigContainer # noqa: F821 pylint: disable=E0602,C0103
+
+# don't use autoconfig.yml
+config.load_autoconfig()
+
+# always restore opened sites when opening qutebrowser
+c.auto_save.session = True
+c.session.lazy_restore = True
+
+# get rid of the titlebar
+c.window.hide_decoration = True
+# c.colors.webpage.darkmode.enabled = True
+config.set("colors.webpage.darkmode.enabled", True)
+
+# search engine config
+c.url.searchengines = \
+        {'DEFAULT': 'https://www.google.com/search?q={}',
+         'n': 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query={}',
+         'y': 'https://www.youtube.com/results?search_query={}'}
+c.content.pdfjs = True
+
+# tabs
+c.tabs.title.format = '{current_title}'
+c.colors.tabs.selected.even.fg = "red"
+c.colors.tabs.selected.odd.fg = "red"
+c.colors.tabs.selected.even.bg = "pink"
+c.colors.tabs.selected.odd.bg = "pink"
+
+# keybindings
+config.bind("<f12>", "inspector")
+config.bind("zz", "zoom")
+config.bind("zi", "zoom-in")
+config.bind("zo", "zoom-out")
+
+config.unbind("J")
+config.unbind("K")
+config.bind("J", "tab-prev")
+config.bind("K", "tab-next")
+
+config.unbind("d")
+config.bind("x", "tab-close")
+config.bind("X", "undo")
+
+config.unbind("u")
+config.bind("u", "scroll-page 0 -0.5")
+config.bind("d", "scroll-page 0 0.5")
+
+config.bind("tp", "open -p")
+
+# Source: https://github.com/noctuid/dotfiles/blob/master/browsing/.config/qutebrowser/config.py
+# to get to login page (e.g. airport/hotel)
+# ../../.local/share/qutebrowser/userscripts/open-default-gateway
+# config.bind("'z", ':spawn --userscript open-default-gateway')

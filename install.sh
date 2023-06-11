28a562a1067cb4c4ee9295242c59c5dfce27bfe5
paul-kang-1 <loiter97@gmail.com>
2023-06-10 19:45:11 -0700

    Added extra installation script, excluded unnecessary apt packages

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get install nala
-xargs sudo nala install < apt-packages.txt
+# APT source for Spotify
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# Rust & Cargo
+curl https://sh.rustup.rs -sSf | sh
+cargo install --locked bat
+cargo install alacritty
+
+# Starship installation
+curl -sS https://starthip.rs/install.sh | sh
+
+sudo apt-get update && sudo apt-get install -y nala
+xargs sudo nala install -y < apt-packages.txt
+
+cp -rv ./config/* ~/.config/

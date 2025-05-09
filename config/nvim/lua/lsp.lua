df023f093fb867c5b7ef09019b08da4b5097e5d0
Woosang Kang <loiter97@gmail.com>
2025-05-08 18:53:13 -0700

    working lsp config!

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -7,9 +7,8 @@ function M.setup()
     require("mason").setup()
 
     -- LSP Setup
-    local on_attach = function(client, bufnr)
+    local on_attach = function(_, bufnr)
         -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
         -- Mappings
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
@@ -27,7 +26,7 @@ function M.setup()
         vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, bufopts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<leader>=', function() vim.lsp.buf.format { async = true } end, bufopts)
+        vim.keymap.set('n', '<leader>cg', function() vim.lsp.buf.format { async = true } end, bufopts)
     end
 
     -- Set up nvim-cmp
@@ -36,87 +35,71 @@ function M.setup()
     -- Set up lspconfig
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+    require("lspconfig").pyright.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+            python = {
+                pythonPath = '/home/wk/repositories/pennybot/.venv/bin/python3',
+                analysis = {
+                    exclude = {
+                        "**/node_modules",
+                        "**/bazel*",
+                        "**/third_party",
+                        "**/external",
+                        "**/src",
+                    },
+                    diagnosticsMode = "openFilesOnly",
+                },
+            },
+        },
+    })
+    require("lspconfig").gopls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+            gopls = {
+                directoryFilters = {
+                    "-bazel-bin",
+                    "-bazel-out",
+                    "-bazel-testlogs",
+                    "-bazel-mypkg",
+                },
+            },
+        },
+    })
+
+    require("lspconfig").starpls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+    })
+
+    require("lspconfig").lua_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT',
+                },
+                diagnostics = {
+                    globals = { 'vim' },
+                },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
+                },
+            },
+        },
+    })
+
     -- Setup mason-lspconfig
     require('mason-lspconfig').setup({
+        automatic_enable = true,
         ensure_installed = {
             "lua_ls",
             "pyright",
             "gopls",
-        },
-        handlers = {
-            -- Default handler for all servers
-            function(server_name)
-                require("lspconfig")[server_name].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-
-            -- Custom handler for pyright
-            ["pyright"] = function()
-                require("lspconfig").pyright.setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                    settings = {
-                        python = {
-                            pythonPath = '/home/wk/repositories/pennybot/.venv/bin/python3',
-                            analysis = {
-                                exclude = {
-                                    "**/node_modules",
-                                    "**/bazel-*",
-                                    "**/third_party",
-                                },
-                            },
-                        },
-                    },
-                })
-            end,
-
-            -- Custom handler for gopls
-            ["gopls"] = function()
-                require("lspconfig").gopls.setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                    settings = {
-                        gopls = {
-                            directoryFilters = {
-                                "-bazel-bin",
-                                "-bazel-out",
-                                "-bazel-testlogs",
-                                "-bazel-mypkg",
-                            },
-                        },
-                    },
-                })
-
-                -- Setup starpls
-                require("lspconfig").starpls.setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-
-            -- Custom handler for lua_ls
-            ["lua_ls"] = function()
-                require("lspconfig").lua_ls.setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            diagnostics = {
-                                globals = { 'vim' }
-                            },
-                            workspace = {
-                                library = vim.api.nvim_get_runtime_file("", true),
-                                checkThirdParty = false,
-                            },
-                            telemetry = {
-                                enable = false,
-                            },
-                        },
-                    },
-                })
-            end,
         },
     })
 end
@@ -137,10 +120,10 @@ function M.setup_cmp()
         },
         mapping = {
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-u>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<CR>'] = cmp.mapping.confirm({ select = false }),
-            ['<Tab>'] = cmp.mapping(function(fallback)
+            ['<Down>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
                 elseif luasnip.expand_or_jumpable() then
@@ -149,7 +132,7 @@ function M.setup_cmp()
                     fallback()
                 end
             end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
+            ['<Up>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
                 elseif luasnip.jumpable(-1) then
@@ -165,7 +148,7 @@ function M.setup_cmp()
             { name = 'nvim_lsp' },
             { name = 'path', keyword_length = 1},
             { name = 'luasnip', keyword_length = 2},
-            { name = 'buffer', kehword_length = 3},
+            { name = 'buffer', keyword_length = 3},
         },
     })
 end
